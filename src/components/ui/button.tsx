a0d7ed2b80b08c627c915b111cd000256f061b7e
Docteur <security@zenth-project.com>
2026-06-16 21:52:21 +0200

    Fix React 19 delete forwardRef - release V0.1.2

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -35,25 +35,20 @@ const buttonVariants = cva(
   }
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean }
+>(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
-
   return (
     <Comp
       data-slot="button"
+      ref={ref as React.Ref<HTMLButtonElement>}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   )
-}
+})
+Button.displayName = "Button"
 
 export { Button, buttonVariants }
